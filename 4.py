a5501cb94f0c409a219fa2e597d3b30f225952ae
Muhammet Eyyüp Tatar <64421764+Kayipruh93@users.noreply.github.com>
2022-03-22 17:36:16 +0300

    Update 4.py

--- 4.py
+++ 4.py
@@ -1,15 +1,16 @@
 import datetime
 from datetime import datetime, timedelta
 
-basla = input("Gün Ay Yıl olacak şekilde bir tarih yazınız: ")  # 14 02 2022
-basla = datetime.strptime(basla, "%d %m %Y")                    # 2022-02-14 00:00:00
-basla = basla.date()                                            # 2022-02-14
+begin = input("Enter a date as day month year: ")  # 14 02 2022
+begin = datetime.strptime(begin, "%d %m %Y")                    # 2022-02-14 00:00:00
+begin = begin.date()                                            # 2022-02-14
 
-baslangictarihi = basla.strftime("%d %m %Y")                    # 14 02 2022
-bitis = basla + timedelta(days=120)                             # 2022-06-14
-bitistarihi = bitis.strftime("%d %m %Y")                        # 14 06 2022
+beginningdate = begin.strftime("%d %m %Y")                    # 14 02 2022
+end = begin + timedelta(days=120)                             # 2022-06-14
 
-simdi = datetime.now().date()                                   # 2022-02-14
-fark = bitis - simdi                                            # 120 days, 0:00:00
+endingdate = end.strftime("%d %m %Y")                        # 14 06 2022
 
-print(f"Virdin başlangıç tarihi {baslangictarihi} bitiş tarihi: {bitistarihi}. Virdin bitmesine {fark.days} gün var.")
+now = datetime.now().date()                                   # 2022-02-14
+days = end - now                                            # 120 days, 0:00:00
+
+print(f"Begins at {beginningdate} and ends at {endingdate}. {days.days} days left.")
